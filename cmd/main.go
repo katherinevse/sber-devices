746440628ddc9ca0aa370860d7f7ab077a930729
katherinevse <katherinevse@mail.ru>
2024-07-18 16:57:11 +0300

    added main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,59 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"log/slog"
 	"os"
+	"sber-devices/internal/testrunner"
 	"strconv"
-	"sync"
 	"time"
 )
 
 func main() {
+	loggerConfig()
 
-	startURL := os.Getenv("START_PAGE")
-	finalURL := os.Getenv("FINAL_PAGE")
+	firstURL := "http://193.168.227.93"
+	finalURL := "http://193.168.227.93/passed"
 
-	//горутины
-	qtyOfThreads, err := strconv.Atoi(os.Getenv("СOUNT_THREADS"))
+	qtyOfThreads, err := strconv.Atoi("5")
 	if err != nil {
-		log.Fatalf("Failed to parse quantity of threads: %v\n", err)
+		log.Fatalf("Failed to parse quantity of threads: %s\n", "5")
 	}
 
-	//RPS
-	maxRPS, err := strconv.Atoi(os.Getenv("MAX_RPS"))
+	maxRPS, err := strconv.Atoi("3")
 	if err != nil {
-		log.Fatalf("Failed to parse RPS parameter: %v\n", err)
+		log.Fatalf("Failed to parse RPS parameter: %s\n", "3")
 	}
 	limiter := time.Tick(getTimeLimit(maxRPS))
 
-	var wg sync.WaitGroup
-	wg.Add(qtyOfThreads)
-
-	for i := 0; i < qtyOfThreads; i++ {
-		go func(clientID int) {
-			defer wg.Done()
-
-			client := &http.Client{}
-			err := Runner(client, startURL, finalURL, limiter)
-			if err != nil {
-				log.Printf("Client %d failed: %v", clientID, err)
-			} else {
-				log.Printf("Client %d successfully completed the testrunner", clientID)
-			}
-		}(i)
-	}
-
-	wg.Wait()
-	fmt.Println("All tests completed")
-
+	testrunner.RunTests(qtyOfThreads, firstURL, finalURL, limiter)
 }
 
-// getTimeLimit возвращает интервал времени между запросами для ограничения RPS
 func getTimeLimit(rps int) time.Duration {
 	if rps <= 0 {
 		return time.Second
 	}
 	return time.Duration(1000/rps) * time.Millisecond
+}
+
+func loggerConfig() {
+	level := slog.LevelInfo
+	err := level.UnmarshalText([]byte(os.Getenv("LOG_LEVEL")))
+	if err != nil {
+		slog.Info("Undefined log level")
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: level,
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, opts)
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 }
